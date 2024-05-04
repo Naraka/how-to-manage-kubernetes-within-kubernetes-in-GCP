701248fa645b5bfe85acddaf5dbda92d160b08f8
Naraka <jcolljeyce@gmail.com>
2024-05-04 13:35:59 +0200

    Small translation into english

--- example.py
+++ example.py
@@ -6,6 +6,6 @@ v1 = client.CoreV1Api()
 namespace = 'NAMESPACE_NAME'
 pods = v1.list_namespaced_pod(namespace)   
 
-print("Pods en el namespace '%s':" % namespace)
+print("Pods in the namespace '%s':" % namespace)
 for pod in pods.items:
     print("%s\t%s\t%s" % (pod.metadata.name, pod.status.phase, pod.status.pod_ip))
